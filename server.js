64ec2fd9a97abf7ea9da3eb2703b7531d394afca
Ken Williams <kenwilliams74@gmail.com>
2020-06-15 15:57:42 -0700

    Changed connection to mongo line in server.js

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ const PORT = 3000;
 
 const app = express();
 
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://user1:password1@ds119702.mlab.com:19702/heroku_2ftmnc1q";
+
 app.use(logger("dev"));
 
 app.use(express.urlencoded({ extended: true }));
@@ -13,9 +15,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(
-  process.env.MONGODB_URI ||
-  "mongodb://user1:password1@ds119702.mlab.com:19702/heroku_2ftmnc1q", 
+mongoose.connect(MONGODB_URI,
   {
     useNewUrlParser: true,
     useUnifiedTopology: true
